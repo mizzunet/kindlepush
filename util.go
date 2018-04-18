72a329441e10b45a0f18faf98dd5fbcdaa7c89f7
zhengchun <zhengchunster@gmail.com>
2018-04-18 11:10:54 +0800

    remove unused method `md5Hash`

--- util.go
+++ util.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"crypto/md5"
-	"encoding/hex"
 	"hash/fnv"
 )
 
@@ -10,10 +8,4 @@ func fnvHash(s string) uint32 {
 	h := fnv.New32a()
 	h.Write([]byte(s))
 	return h.Sum32()
-}
-
-func md5Hash(s string) string {
-	h := md5.New()
-	h.Write([]byte(s))
-	return hex.EncodeToString(h.Sum(nil))
 }
